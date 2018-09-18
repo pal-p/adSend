6e6600859e99d46831dd8977bdb63f6cea8ada5f
Palwasha Pordeli <palwashap04@gmail.com>
2018-09-18 17:51:18 +1000

    oauth ,google client id/secret, passport

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 //#create our express server and new route handler.it is the root file.create express application and logic to handle requests here.
 const express = require('express');
 
+//require passport and google strategy for oauth
+const passport = require('passport');
+const GoogleStrategy = require ('passport-google-oauth20').Strategy;
+
+//get keys for google strategy use
+const keys =require('./config/keys');
+
+
+
+
 //create the exp application can have many in one node js proj.gets the req from node and hands to handlers...
 const app =express(); 
 
-//route handler
-app.get('/', (req,res) =>{
+//tell pasport how to use google strategy.pass config args inside.2 options client id and client secret, provided by google oath service (keep them secure!).
+passport.use( 
+ new GoogleStrategy({
+   clientID: keys.googleClientId,
+   clientSecret: keys.googleClientSecret,
+   callbackURL: '/auth/google/callback'//this is the url user is redirected to our srvr after giving premision 
+ }, accessToken => {
+    console.log(accessToken);
+    
+ })
+);
 
-   res.send({hello: 'buddy wwoa! go'});
-});
+
+//now test oath
+//2nd arg says involve passport and auth by passport's 'google' strategy.the string 'google' is built in identifier of GoogleStrategy. 2nd arg scope specifies to google srvr what we want to access about user.
+app.get(
+	'/auth/google',
+	passport.authenticate('google',{
+ 	  scope: ['profile', 'email']
+        })
+);
+
+
+
+
+
+
+
+
+
 
 //listen on port 3000
 //app.listen(3000);
