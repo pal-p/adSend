b932c99f0181019aac50d11b27f703cf68b87aed
Palwasha Pordeli <palwashap04@gmail.com>
2018-09-19 22:16:08 +1000

    prod and dev envs seperated typo error was fixed

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //export code and make it available to other files
 
 module.exports ={
-   googleClientID: process.env.GOOGLE_CLIENT_ID,
+   googleClientId: process.env.GOOGLE_CLIENT_ID,
    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
    mongoUri: process.env.MONGO_URI,
    cookieKey: process.env.COOKIE_KEY
