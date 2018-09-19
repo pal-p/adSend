f5a4be38ec71ec6e3e0479fcce57bcbf5a9d6414
Palwasha Pordeli <palwashap04@gmail.com>
2018-09-19 20:59:05 +1000

    dev and prod envs seperated

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientId: '1057327676345-2r2thksdel7u2cdfontc0qq580nu8igd.apps.googleusercontent.com', 
+  googleClientSecret:'jfsUHv9DaZIUtM3dVevGpFOn',
+  mongoURI:'mongodb://palAdsend:ojanak2018@ds161322.mlab.com:61322/adsenddev',
+  cookieKey: 'fdjjdjdjdjdfdjdydytrrrythgcvhgmnlkpop'
+};
+//dont commit this!
