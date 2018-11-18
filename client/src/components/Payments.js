baf9eaf7ffbf2cfa3146ba304ffa9f50f1c4c675
Palwasha Pordeli <palwashap04@gmail.com>
2018-11-18 13:15:05 +1100

    request backend to add credits for user

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 class Payments extends Component{
    render(){
       //debugger;
@@ -10,7 +11,7 @@ class Payments extends Component{
              name="adSender"
              description="Pay $5 for 5 email credits"
              amount={500}
-             token={token => console.log(token)}
+             token={token => this.props.handleToken(token)}
              stripeKey={process.env.REACT_APP_STRIPE_KEY}
          >
             <button className="btn" style ={{backgroundColor: '#5c0303'}}>
@@ -22,4 +23,4 @@ class Payments extends Component{
    }
 
 }
-export default Payments;
+export default connect(null, actions)(Payments);
