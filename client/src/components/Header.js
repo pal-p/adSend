8049b71b391449bf9baae8e749e61ee75f802bbf
Palwasha Pordeli <palwashap04@gmail.com>
2018-11-18 22:35:36 +1100

    styled credit text in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,9 +15,11 @@ class Header extends Component {
                  <li key="1">
                    <Payments/>
                  </li>,
-                 <li key ="3"> Credits: {this.props.auth.credits}
+                 <li key ="3" style={{color:'#5c0303', margin: '0 10px'}}> 
+                     Credits: {this.props.auth.credits}
                  </li>,
-                 <li key="2"><a href="/api/logout" style={{color:'#5c0303'}}>Logout</a>
+                 <li key="2"><a href="/api/logout" style={{color:'#5c0303'}}>
+                     Logout</a>
                  </li>
                  
                  ];
