fa531a4e8ad01e45a6a79eabbd09ca7a1db0dbc2
Palwasha Pordeli <palwashap04@gmail.com>
2018-12-16 15:37:02 +1100

    improved email template

--- services/emailTemplates/advertisementTemplate.js
+++ services/emailTemplates/advertisementTemplate.js
@@ -1,5 +1,18 @@
-module.exports = (survey) => {
+module.exports = (advertisement) => {
 
-  //return '<div> Hello there!</div>'
-  return '<div>'+survey.body+'</div>'
+   //return '<div> Hello there!</div>'
+  // return '<div>'+advertisement.body+'</div>'
+  return `<html>
+         <body>
+            <div style="text-align: center">
+              <h1 style="color: green"> Super Sale!</h1>
+              <h2 style="color: tomato"> ${advertisement.title}</h2>
+              <p style="color: green"> ${advertisement.body}</p>
+              <div>
+                 <a href="http://localhost:3000" style="color: red"> Shop Now</a>
+                  
+              </div>
+            </div>
+         </body>
+       </html>`
 };
