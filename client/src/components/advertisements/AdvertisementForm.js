00ee07c9b9460ba7125bf37a2ad3621d3b22847e
Palwasha Pordeli <palwashap04@gmail.com>
2018-12-19 12:03:58 +1100

    used AdvertisementField component for Field

--- client/src/components/advertisements/AdvertisementForm.js
+++ client/src/components/advertisements/AdvertisementForm.js
@@ -1,18 +1,23 @@
 import React , {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
-class AdvertisementForm extends Component {
+import AdvertisementField from './AdvertisementField';
 
+class AdvertisementForm extends Component {
+  
+  renderFields (){
+    return (
+       <div>
+        <Field type="text" name="title" component={AdvertisementField} /> 
+       </div>
+    );
+  }
   render(){
 
     return(
       <div> 
         adform!!!
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field
-             type="text"
-             name="adTitle"
-             component="input"
-          />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </ form>
       </div>
