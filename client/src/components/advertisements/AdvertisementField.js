51f46d365cd75b4ce960fb138fc23d6a636c7680
Palwasha Pordeli <palwashap04@gmail.com>
2018-12-20 11:51:37 +1100

    showing errors only if fields are touched

--- client/src/components/advertisements/AdvertisementField.js
+++ client/src/components/advertisements/AdvertisementField.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-export default ({input, label, meta})=>{
+export default ({input, label, meta:{error, touched}})=>{
 
-  console.log(meta); 
+  
+  //console.log(meta); 
  // console.log(props);
+  console.log(touched);
   return(
      <div style={{ backgroundColor: '#e1d084'}}>
          
          <label style={{color:'#5c0303'}}>{label}</label>
          <input {...input}  />
-         <div> {meta.error} </div>
+         <div> {touched && error} </div>
      </div>
   );
 
