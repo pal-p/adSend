f9ecfa4e5e72363506cb47499672cce2360bbfab
Palwasha Pordeli <palwashap04@gmail.com>
2018-12-20 14:42:42 +1100

    css improvement for landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import myLogo from '../email-sending.png';
 
 const Landing = ()=>{
 
    return(
-     <div style={{textAlign: 'center', backgroundColor:'#ddc974'}}>
+     <div style={{textAlign: 'center', backgroundColor:'#ffcc80'}}>
        <h1 style={{color:'#5c0303', fontWeight:'bold'}}>
           adSender
        </h1>
        <h3 style={{color:'#5c0303'}}> 
          Email Marketing
        </h3>
-       
+       <div>
+          <img src={myLogo} alt="MyLogo" />;
+       </div>
+        
      </div>
 
    );
