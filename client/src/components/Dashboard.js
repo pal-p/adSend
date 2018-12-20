4c41203654d3efeaa4260a45723a9e492d62e04e
Palwasha Pordeli <palwashap04@gmail.com>
2018-12-20 11:06:35 +1100

    fixed className typo in dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,9 +4,9 @@ const Dashboard = () => {
   return (
    <div style={{ backgroundColor: '#e1d084'}
 }> Dashboard ! ! 
-    <div class="fixed-action-btn">
-      <Link to="/dashboard/new" class="btn-floating btn-large red">
-      <i class="material-icons">add</i>
+    <div className="fixed-action-btn">
+      <Link to="/dashboard/new" className="btn-floating btn-large red">
+      <i className="material-icons">add</i>
      </Link>
     </div> 
    </div>
