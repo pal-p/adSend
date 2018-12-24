ff686ee00f5eb0a689a3c82c0c1ede04ab02893d
Palwasha Pordeli <palwashap04@gmail.com>
2018-12-24 13:39:45 +1100

    redirect user from email to the sales url

--- client/src/components/advertisements/formFields.js
+++ client/src/components/advertisements/formFields.js
@@ -2,6 +2,7 @@ export default [
   { label:'Ad Title', name:'title',noValueError: 'You must provide a title'},
   { label:'Subject Line', name:'subject', noValueError: 'You must provide a subject'},
   { label:'Email Body', name:'body', noValueError: 'You must provide a body'},
+  { label:'Sales URL', name:'salesUrl', noValueError: 'You must provide sales url'},
   { label:'Recipient List', name:'recipients', noValueError: 'You must provide emails'}
   
 ];
